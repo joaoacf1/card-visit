f8e2062aa43da31382d4cb7dc872949a2a6f2a2b
JOAO <joaoantoniocorreaf@gmail.com>
2023-06-24 19:26:59 -0300

    Mudando o avatar ao trocar o modo

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function toggleMode() {
     const html = document.documentElement;
     html.classList.toggle("light-mode");
+
+    const img = document.querySelector("#avatar img");
+
+    if (html.classList.contains('light-mode')) {
+        img.setAttribute("src", "./assets/avatar-light-mode.jpg");
+        img.setAttribute("alt", "Foto de João Antônio sorrindo com óculos escuros");
+    }
+    else {
+        img.setAttribute("src", "./assets/avatar-joao.jpg");
+        img.setAttribute("alt", "Foto de João Antônio sorrindo");
+    }
 }
-
-
